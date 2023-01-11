d22507c5541b3e169877e391b99dba342b37e956
JungHoyun <hooyn_industry@naver.com>
2023-01-11 10:18:25 +0900

    react crud

---
+++ src/components/PrintLine.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PrintLine extends Component {
+    render() {
+        return (
+            <div>
+                <br />
+                <hr />
+            </div>
+        );
+    }
+  }
+
+export default PrintLine;
